4de430827b0f92d349f9f273b60d5a05dbcdb4de
tsukishima <hir01994o915@gmail.com>
2018-11-18 20:15:47 +0900

    Contentモデルの作成とマイグレーション

---
+++ db/migrate/20181118084520_create_contents.rb
@@ -0,0 +1,11 @@
+class CreateContents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contents do |t|
+      t.text      :title, null: false
+      t.text      :summary
+      t.text      :text
+      t.text      :image
+      t.timestamps
+    end
+  end
+end
