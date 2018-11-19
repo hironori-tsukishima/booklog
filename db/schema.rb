4c18f165ddacdaf72ad5af5d97b70518721954fb
tsukishima <hir01994o915@gmail.com>
2018-11-19 21:56:27 +0900

    contentテーブルへのuser_idのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181119081538) do
+ActiveRecord::Schema.define(version: 20181119084451) do
 
   create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",      limit: 65535, null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20181119081538) do
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_contents_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -29,8 +31,10 @@ ActiveRecord::Schema.define(version: 20181119081538) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "contents", "users"
 end
