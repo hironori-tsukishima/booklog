304a9268fbcef6bc3da9c9044741cd55edbdc9db
tsukishima <hir01994o915@gmail.com>
2018-11-19 12:46:25 +0900

    コントローラの設定

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,26 @@
 class ContentsController < ApplicationController
+
+  def index
+    @contents = Content.order("created_at ASC")
+  end
+
+  def new
+  end
+
+  def create
+    @content = Content.new
+      if @content.save
+        redirect_to contents_path
+      else
+        render new_content_path
+      end
+  end
+
+  def content_params
+    params.require(:content).permit(:image, :text, :title, :summary,)
+  end
+
 end
+
+
+
