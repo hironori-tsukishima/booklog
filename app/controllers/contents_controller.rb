acf1618cac2f37e9bb5dfa7a0c195ac70a7f64a8
tsukishima <hir01994o915@gmail.com>
2018-11-20 13:51:53 +0900

    Flashメッセージの作成

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -11,9 +11,9 @@ class ContentsController < ApplicationController
   def create
     @content = Content.new(content_params)
       if @content.save
-        redirect_to contents_path
+        redirect_to contents_path, notice:"保存しました！"
       else
-        render :new
+        render :new,flash.now[:alert] = "保存できていません。タイトルは入れてくださいね"
       end
   end
 
@@ -29,9 +29,9 @@ class ContentsController < ApplicationController
     content = Content.find(params[:id])
       if content.user_id == current_user.id
           content.update(content_params)
-            redirect_to content_path
+            redirect_to content_path, notice:"変更を保存しました"
       else
-        render :index
+        render :index,flash.now[:alert] = "変更は保存できていません、、。"
       end
   end
 
@@ -39,9 +39,9 @@ class ContentsController < ApplicationController
     content = Content.find(params[:id])
       if content.user_id == current_user.id
         content.destroy
-          redirect_to contents_path
+          redirect_to contents_path,notice:"削除しました"
       else
-        render :show
+        render :show,flash.now[:alert] = "削除できませんでした"
       end
   end
 
